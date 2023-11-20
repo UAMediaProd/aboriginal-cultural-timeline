fb19ca4e9f1cea6371ec381d2d76f7db2508748f
Hui Mu <hui.mu@adelaide.edu.au>
2023-11-20 12:16:31 +1030

    update timeline styles; update with new data

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,38 @@
   <main class="px-32">
     <div class="snap-end">
       <div class="flex justify-center text-center pt-3">
-        <h1 class="max-w-[650px] my-6">A Timeline of Colonisation of the First Nations of Australia</h1>
+        <h2 class="max-w-[500px] my-6">{{ TITLE }}</h2>
       </div>
-      <div class="text-center my-3">[intro text]</div>
-      <div class="text-center my-3 text-lg font-bold">Scroll / Click Here to begin</div>
-      <div class="text-center my-3 cursor-pointer" @click="scrollToSection"><i class="fas fa-arrow-circle-down text-3xl" /></div>
+      <div v-for="line in INTRO_TEXT" class="max-w-[750px] italic text-center leading-6 mb-3 mx-auto">{{ line }}</div>
     </div>
 
     <!--  Timeline section  -->
     <div class="relative">
-      <div :id="'era-'+ era.id" v-for="era in ERA_DATA" class="text-center z-10">
-        <div class="bg-white py-2 snap-start">
-          <div class="w-full bg-slate-100 py-3 px-6">
-            <div class="text-xl font-bold">{{ era.name }}</div>
-            <div>{{ era.summary }}[summary]</div>
+      <div class="bg-white text-center mt-8 pb-4 mb-24">
+        <div class="text-lg font-bold rounded border-2 border-brand-midblue text-brand-midblue hover:text-white hover:bg-brand-midblue cursor-pointer mx-auto w-fit px-4 py-2" @click="scrollToSection">Scroll / Click to Begin</div>
+      </div>
+      <div :id="'era-'+ era.id" v-for="era in ERA_DATA" class="text-center z-10" :style="{paddingBottom: era.gapAfter * 50 + 'px'}">
+        <div v-if="!era.isSubEra" class="text-center bg-white">
+          <i class="fas fa-triangle rotate-180 text-gray-200 text-lg align-top mb-auto -mt-1.5" />
+        </div>
+        <div v-if="!era.isSubEra" class="bg-white pb-6 pt-2 snap-start">
+          <div class="w-full bg-brand-darkblue text-white py-3 px-6">
+            <div class="text-2xl font-bold my-2">{{ era.name }}</div>
+            <p v-for='paragraph in era.summary' class="text-left italic text-base text-slate-200 leading-6 mb-3">{{ paragraph }}</p>
+          </div>
+        </div>
+        <div v-else class="snap-start">
+          <hr class="border-2 border-dashed border-gray-200" />
+          <div class="w-1/2 py-3 px-6">
+            <div class="text-right text-2xl font-bold my-2">{{ era.name }}</div>
+            <p v-for='paragraph in era.summary' class="text-right italic text-base leading-6 mb-3">{{ paragraph }}</p>
           </div>
         </div>
         <div v-for="(year, index) in YEAR_DATA.filter(y => y.eraId === era.id)" class="pb-1">
-          <div :style="{paddingTop: year.gap * 60 + 'px'}" />
-          <div :id="`year-${era.id}-${index}`" class="relative flex w-1/2 py-1 snap-start" :class="index%2 ? 'flex-row-reverse justify-start ml-[50%] pl-2' : 'text-right justify-end ml-[50%] -translate-x-full pr-2'">
-            <div class="w-full bg-blue-50 rounded-xl px-4 py-2" :class="index%2 ? 'text-left' : 'text-right'">
-              <div class="font-bold text-blue-600 text-2xl">{{ year.yearText }}</div>
-              <div class="font-bold text-xl">{{ year.name }}</div>
-              <div class="">[text]</div>
+          <div :id="`year-${era.id}-${index}`" class="relative flex w-1/2 py-1 snap-start" :class="index%2 ? 'flex-row-reverse justify-start ml-[50%] pl-2' : 'text-right justify-end ml-[50%] -translate-x-full pr-2'" :style="{marginTop: year.gap * 10 + 'px'}">
+            <div class="w-full bg-brand-midblue bg-opacity-10 rounded-xl p-3" :class="index%2 ? 'text-left' : 'text-right'">
+              <div class="font-bold text-brand-midblue text-2xl">{{ year.yearText }}</div>
+              <p v-for="line in year.details" class="text-base leading-5 mt-2">{{ line }}</p>
             </div>
             <div class="h-0.5 w-[100px] bg-black my-auto mx-2"></div>
             <div class="absolute top-1/2 -translate-y-1/2 text-center text-xl" :class="index%2 ? '-left-[10px]' : '-right-[10px]'">
@@ -34,15 +43,18 @@
       </div>
 
       <div class="absolute left-1/2 top-0 -z-10 -translate-x-1/2 w-1 h-full bg-gray-200" />
+      <div class="text-center bg-white">
+        <i class="fas fa-triangle rotate-180 text-gray-200 text-lg align-top mb-auto -mt-1.5" />
+      </div>
     </div>
 
     <!--  side nav  -->
-    <div class="fixed flex flex-col justify-between top-[10%] left-0 w-28 h-[80%] px-2 py-4 rounded-r-xl text-center text-white font-bold bg-black bg-opacity-70">
+    <div class="fixed flex flex-col justify-between top-[10%] left-0 w-28 px-2 py-4 rounded-r-xl text-center text-white font-bold bg-black bg-opacity-70">
       <div>65,000 BCE</div>
-      <div class="relative h-[400px]">
-        <input id="VerticalSlider" v-model="currentSection" type="range" min="0" :max="sectionIds.length - 1" step="1" class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 left-1/2 w-[360px] h-1 bg-gray-200 rounded-lg appearance-none cursor-pointer rotate-90" @change="scrollToSection">
+      <div class="relative h-[40vh] my-1">
+        <input id="VerticalSlider" v-model="currentSection" type="range" min="0" :max="sectionIds.length - 1" step="1" class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 left-1/2 w-[40vh] h-1 bg-gray-200 rounded-lg appearance-none cursor-pointer rotate-90" @change="scrollToSection">
       </div>
-      <div>2023</div>
+      <div>Present</div>
       <div class="cursor-pointer" @click="prevSection">
         <i class="fas fa-chevron-up px-6 py-4 bg-black rounded-xl my-1" /></div>
       <div class="cursor-pointer" @click="nextSection">
@@ -53,7 +65,7 @@
   </main>
 </template>
 <script setup>
-import { ERA_DATA, YEAR_DATA } from "@/assets/data"
+import { INTRO_TEXT, ERA_DATA, YEAR_DATA, TITLE } from "@/assets/data"
 import { computed, onBeforeUnmount, onMounted, ref } from "vue";
 
 const currentSection = ref(0)
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,27 +1,271 @@
+export const TITLE = 'A Timeline of Colonisation of the First Nations of Australia'
+
+export const INTRO_TEXT = [
+  'The process of British colonisation, dispossession, and genocide against the Aboriginal and Torres Strait Islanders was long and complex.',
+  'This timeline is an attempt to introduce readers to a general overview of colonisation and is not intended to be comprehensive or definitive.'
+]
+
 export const ERA_DATA = [
-  { id: 0, name: 'Pre-Colonisation', summary: ''},
-  { id: 1, name: 'Invasion and Early Colonisation', summary: ''},
-  { id: 2, name: '"Protection" Era', summary: ''},
-  { id: 3, name: 'The Stolen Generations', summary: ''},
-  { id: 4, name: 'Assimilation and Activism', summary: ''},
-  { id: 5, name: 'Integration and Activism', summary: ''},
-  { id: 6, name: 'Reconciliation and Self-Determination', summary: ''},
+  {
+    id: 0,
+    name: 'Aboriginal Land',
+    yearText: '65,000 BCE - 1770',
+    isSubEra: false,
+    gapAfter: 3,
+    summary: [
+      'Aboriginal and Torres Strait Islander people have inhabited the continent of Australia and its surrounding islands for at least 65,000 years. Prior to British colonisation, there were an estimated 260 distinct Aboriginal and Torres Strait Islander language groups with up to 500 different dialects. These language groups developed rich and intricate cultural lives based on storytelling, ritual, ceremony, art, law, language, and spirituality – all of which were built upon a deep connection to their respective Country.',
+      'Colonial ignorance has led to oversimplifications and misunderstandings about traditional Aboriginal life. Contrary to some common beliefs, Aboriginal societies were not ‘mere’ hunter-gatherers. They held a sophisticated knowledge of how to sustainably live off and manage the land. Practices included harvesting, storing, and planting seeds, building dams, wells, irrigation systems, and elaborate traps, and an advanced usage of fire burnoffs to protect and maintain the land. They built stone houses, painted with ochre, and buried and cremated their dead. Aboriginal people lived in harmony with the natural environment, allowing civilisation to flourish and maintaining the wild beauty of nature for over 60,000 years.'
+    ]
+  },
+  {
+    id: 1,
+    name: 'Invasion and Colonisation',
+    yearText: '1770-1890',
+    isSubEra: false,
+    gapAfter: 1,
+    summary: [
+      'There were over 250 distinct Aboriginal and Torres Strait Islander language groups when Lieutenant James Cook first arrived on the continent',
+      'Early colonisers could not understand the Aboriginal peoples’ sustainable, harmonious connection to the land. This justified the settlers’ wrongful concept of ‘terra nullius’, or ‘land belonging to nobody’ -- land that was ripe for colonisation.',
+      'There were an estimated 300,000-950,000 Aboriginal and Torres Strait Islander people at the time of invasion. By 1900, this number had reduced by 90%. New diseases, food scarcity, forcible land acquisition, and direct violent conflicts all contributed to the decline.'
+    ]
+  },
+  {
+    id: 2,
+    name: 'Frontier Wars',
+    yearText: '',
+    isSubEra: true,
+    gapAfter: 1,
+    summary: [
+      '\'Frontier Wars\' refer to all the massacres, wars, and instances of resistance beginning from the arrival of colonists and ending roughly around 1934.',
+      'Below is a map of all the recorded colonial frontier massacres in Australia in this period.'
+    ]
+  },
+  {
+    id: 3,
+    name: '"Protection" and Segregation',
+    yearText: '1860-1900',
+    isSubEra: false,
+    gapAfter: 1,
+    summary: [
+      'By the mid 1800s, the violence, disease and dispossession resulting from colonisation had reduced the Aboriginal and Torres Strait Islander population. Laws were put in place to assimilate Aboriginal and Torres Strait Islander people. It was assumed that their culture, lifestyle, and identity would eventually \'die out\' or \'fade away\'.',
+      'Survivors of frontier wars were moved onto missions and reserves to safeguard, control, and Christianise them. Living conditions on reserves were often harsh. They were told where they could live, work, and travel, who they could associate with, and who they could marry. These policies were paternalistically called \'protection\'.'
+    ]
+  },
+  {
+    id: 4,
+    name: 'Assimilation',
+    yearText: '1910-1950s',
+    isSubEra: false,
+    gapAfter: 1,
+    summary: [
+      'Assimilation was an official federal policy intended to erase Aboriginal and Torres Strait Islanders\' identity and absorb them into white society. Many reserves closed and residents were encouraged to move into the towns. This caused significant stress to many who had grown to view the reserves as homelands and had built strong community ties there.',
+      'Aboriginal people were subject to continued control over their lives, including curfews, lower wages, and alcohol bans. Most significant of all was the widespread policy of forced child removal, also know as the Stolen Generations.',
+      ' In the 1920s and 1930s, Aboriginal activism boomed. There were increasing calls for the abolition of the Aboriginal Protection Board in favour of an all-Aboriginal governing body.'
+    ]
+  },
+  {
+    id: 5,
+    name: 'The Stolen Generations',
+    yearText: '1910s-1970s',
+    isSubEra: false,
+    gapAfter: 1,
+    summary: [
+      'The Stolen Generations refer to the forcible removal of Aboriginal and Torres Strait Islander children from their families. Children were taken based on the assimilation polices which claimed that the lives of Aboriginal children would be improved if they became part of white society. This process was also called ‘natural elimination’.',
+      'Children taken were denied access to their families, communities, cultures and taught to reject their heritage in favour of the white culture. Names were changed and language was forbidden.',
+      'An estimation of 10-33% of Aboriginal and Torres Strait children were separated from their familied in just 60 years.  Intergenerational trauma has continued to severely impact those taken and their descendents.',
+      'In 1980, government link-up services were established to support members of the Stolen Generations in their search to reconnect with their families. Testimonies of the Stolen Generations can be heard and read here: https://www.stolengenerationstestimonies.com/'
+    ]
+  },
+  {
+    id: 6,
+    name: 'Activism and Integration',
+    yearText: '1960s-1990s',
+    isSubEra: false,
+    gapAfter: 1,
+    summary: [
+      'The 1960s were a period of significant social change. Aboriginal and Torres Strait Islander people tirelessly campaigned for voting rights, self-determination, equal pay, and better working and living conditions. Their efforts culminated in the 1967 Referendum. Commonwealth authorities increasingly adopted measures to improve the welfare and outcomes of Aboriginal people and grant land rights to traditional owners.',
+      'Although much progress was made in this period, calls for constitution recognition and a Treaty did not eventuate. Aboriginal and Torres Strait Islander people still experienced much hardship, racism, and land dispossession.'
+    ]
+  },
+  {
+    id: 7,
+    name: 'Reconciliation and Self-Determination',
+    yearText: '1991-present',
+    isSubEra: false,
+    gapAfter: 1,
+    summary: [
+      'Instead of a Treaty, the Australian Government formed an official policy of Reconciliation. Its goal was to strengthen relationships between First Nations and non-First Nations people through increased understanding and respect. Reconciliation still remains a major commitment for many workplaces and organisations.',
+      'It is now recognised that Aboriginal and Torres Strait Islander people must be centered in decision-making about their own lives. More Aboriginal-controlled organisations and initiatives have arisen over recent years, including health care, education, and social services. Terra nullius, the doctrine that justified British invasion, was finally nullified in favour of recognising the Aboriginal and Torres Strait Islanders as the original owners of the land.',
+      'Although enormous strides have been made over the last few decades, many issues remain. There is still no Treaty, and Aboriginal and Torres Strait Islander representation is still not protected by the Constitution.'
+    ]
+  },
 ]
 
 export const YEAR_DATA = [
-  { eraId:0, gap: 1, yearText: '65,000 BCE', name: 'Earliest evidence of civilisation', description: ''},
-  { eraId:0, gap: 1, yearText: '47,000 - 43,000 BCE', name: 'Early technologies', description: ''},
-  { eraId:0, gap: 1, yearText: '38,000 BCE', name: 'Oldest known cremation', description: ''},
-  { eraId:0, gap: 1, yearText: '11,000 BCE', name: 'Tasmania isolated', description: ''},
-  { eraId:0, gap: 1, yearText: '6,000 BCE', name: 'Torres Strait Islands formed', description: ''},
-  { eraId:0, gap: 5, yearText: '1588', name: 'Trade with Makassar', description: ''},
-  { eraId:0, gap: 1, yearText: '1606', name: 'First Dutch Landing', description: ''},
-  { eraId:0, gap: 1, yearText: '1697', name: 'William Dampier lands', description: ''},
+  {
+    eraId: 0,
+    gap: 2,
+    yearText: '65,000 BCE',
+    name: '',
+    details: ['Earliest known evidence of Aboriginal civilisation']
+  },
 
-  { eraId:1, gap: 1, yearText: '1770', name: 'Botany Bay landing', description: ''},
-  { eraId:1, gap: 1, yearText: '1788', name: 'First Fleet arrives', description: ''},
-  { eraId:1, gap: 1, yearText: '1789', name: 'Smallpox outbreak', description: ''},
-  { eraId:1, gap: 1, yearText: '1792', name: 'Pemulwuy\'s resistance', description: ''},
-  { eraId:1, gap: 1, yearText: '1794', name: 'Frontier massacres', description: ''},
-  { eraId:1, gap: 1, yearText: '1804', name: 'Mounting violence', description: ''},
+  {
+    eraId: 1,
+    gap: 5,
+    yearText: '1770',
+    name: '',
+    details: ['James Cook arrives at Botany Bay and claims possession of Australia for Britain']
+  },
+  {
+    eraId: 1,
+    gap: -5,
+    yearText: '1788',
+    name: '',
+    details: ['The First Fleet arrives in Sydney Harbour on January 24, 1788']
+  },
+  {
+    eraId: 1,
+    gap: 1,
+    yearText: '1834',
+    name: '',
+    details: [
+      'South Australia established as a British colony.',
+      'The Letters Patent declare that Aboriginal occupation of lands must not be interrupted; this is ignored in practice.'
+    ]
+  },
+
+  {
+    eraId: 2,
+    gap: 3,
+    yearText: '1824-1830',
+    name: '',
+    details: ['Tasmanian \'Black War\'. Aboriginal population forcibly located to Flinders Island, where most die due to poor conditions.']
+  },
+  {
+    eraId: 2,
+    gap: -5,
+    yearText: '1838',
+    name: '',
+    details: ['Myall Creek massacre. 28 Aboriginal people, mostly women and children, are murdered by white stockmen.']
+  },
+
+  {
+    eraId: 3,
+    gap: 3,
+    yearText: '1860',
+    name: '',
+    details: ['Official enquiry into Aboriginal welfare recommends the creation of missions, reserves, and a Board to control and regulate Aboriginal lives.']
+  },
+  {
+    eraId: 3,
+    gap: -6,
+    yearText: '1881',
+    name: '',
+    details: ['Parliamentary inquiry into poor conditions at Corranderrk Reserve. Aboriginal residents call for land rights and self-determination.']
+  },
+  {
+    eraId: 3,
+    gap: 1,
+    yearText: '1901',
+    name: '',
+    details: [
+      'Australia becomes a Federation. Aboriginal people are excluded from the Constitution.'
+    ]
+  },
+
+  {
+    eraId: 4,
+    gap: 3,
+    yearText: '1911',
+    name: '',
+    details: ['Aboriginies Act 1911 (SA) passed. Removal of \'mixed descent\' children begins.']
+  },
+  {
+    eraId: 4,
+    gap: 1,
+    yearText: '1938',
+    name: '',
+    details: ['Aboriginal Day of Mourning and Protest held in Sydney on January 26th. Eventually becomes a week-long celebration in July, now known as NAIDOC Week.']
+  },
+  {
+    eraId: 4,
+    gap: 1,
+    yearText: '1956',
+    name: '',
+    details: [
+      'Britain begins atomic weapons testing program in Australia.',
+      'Aboriginal communities severely impacted by fallout, contamination, and displacement, still suffer higher cancer rates than the general population.'
+    ]
+  },
+
+  {
+    eraId: 6,
+    gap: 3,
+    yearText: '1966',
+    name: '',
+    details: [
+      'Gurindji people at Wave Hill station begin their seven-year-long strike, calling for better working conditions, fair wages, and reclamation of country. Their petition is ignored.',
+    'In 1974, the Labor Government  would finally return their lands in a symbolic ceremony. Prime Minister Gough Whitlam poured a handful of soil through Vincent Lingiari’s fingers, saying:',
+    '“Vincent Lingiari, I solemnly hand to you these deeds as proof, in Australian law, that these lands belong to the Gurindji people".',
+    'This moment has been celebrated through song, most recently by Electric Fields.'
+    ]
+  },
+  {
+    eraId: 6,
+    gap: -16,
+    yearText: '1967',
+    name: '',
+    details: ['Over 90% of Australians vote YES in referendum to amend the Constitution. Aboriginal and Torres Strait Islander people included in Census for the first time.']
+  },
+  {
+    eraId: 6,
+    gap: 3,
+    yearText: '1985',
+    name: '',
+    details: ['Uluru handed back to the Anangu people after decades of lobbying.']
+  },
+  {
+    eraId: 6,
+    gap: -5,
+    yearText: '1988',
+    name: '',
+    details: ['Barunga Statement presented to Prime Minister Bob Hawke, calling for land rights, self-determination, and compensation. Hawke\'s promise of a treaty by 1990 has still not been fulfilled.']
+  },
+
+  {
+    eraId: 7,
+    gap: 3,
+    yearText: '1992',
+    name: '',
+    details: ['High Court of Australia\'s Mabo decision formally recognises native title and renders terra nullius a legal fiction.']
+  },
+  {
+    eraId: 7,
+    gap: 1,
+    yearText: '1997',
+    name: '',
+    details: ['\'Bringing Them Home\' report released by the National Inquiry into the Separation of Aboriginal and Torres Strait Island Children from Their Families. Most of its 54 recommendations are ignored.']
+  },
+  {
+    eraId: 7,
+    gap: 3,
+    yearText: '2008',
+    name: '',
+    details: ['Prime Minister Kevin Rudd formally apologises on behalf of the Government for creating the Stolen Generations.']
+  },
+  {
+    eraId: 7,
+    gap: 1,
+    yearText: '2017',
+    name: '',
+    details: ['Uluru Statement from the Heart born from a series of regional dialogues held across the country.']
+  },
+  {
+    eraId: 7,
+    gap: 1,
+    yearText: '2023',
+    name: '',
+    details: ['Australia votes no in the Voice to Parliament referendum, denying Aboriginal and Torres Strait Islander communities an enshrined route to help inform policy and legal decision that impact their lives. This voice has been called for close to 100 years.']
+  }
 ]
